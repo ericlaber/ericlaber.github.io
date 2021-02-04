5ff606421e2a04ac3b6c1c6f94f2511d92b7efcb
ericlaber <73533014+ericlaber@users.noreply.github.com>
2021-02-03 21:49:29 -0500

    Add files via upload

--- docs/allSubsets.R
+++ docs/allSubsets.R
@@ -2,30 +2,30 @@
 ## @file allSubsets.R
 ## @brief Illustrate all subsets variable selection
 ## @author Eric B. Laber
-## @note For big data
+## @note Written hastily and rather badly 
 ##
-require ("lasso2");
-require ("MASS");
-require ("leaps");
+require ("lasso2")
+require ("MASS")
+require ("leaps")
 
 ## Load data fit OLS estimate
-data ("Prostate");
-head (Prostate);
+data ("Prostate")
+head (Prostate)
 
-Prostate = data.frame (scale (Prostate, center=T, scale=F));
-lmFit = lm (lpsa ~ ., data=Prostate);
-summary (lmFit);
+Prostate = data.frame (scale (Prostate, center=T, scale=F))
+lmFit = lm (lpsa ~ ., data=Prostate)
+summary (lmFit)
 
 ## All subsets regression 
-dMat = as.matrix (Prostate[,1:8]);
-leapsFit = regsubsets (x = dMat, y=Prostate$lpsa, nbest=1, nvmax=8);
+dMat = as.matrix (Prostate[,1:8])
+leapsFit = regsubsets (x = dMat, y=Prostate$lpsa, nbest=1, nvmax=8)
 
-summary (leapsFit);  # examine models of each size
+summary (leapsFit)  # examine models of each size
 
-plot(summary(leapsFit)$bic, type="l", xlab="df", ylab="bic");
+plot(summary(leapsFit)$bic, type="l", xlab="df", ylab="bic")
 
 
 ## Stepwise selection using BIC
-n = dim(dMat)[1];
-lmStep = stepAIC (lmFit, k=log(n));  # same as all subsets!
+n = dim(dMat)[1]
+lmStep = stepAIC (lmFit, k=log(n))  # same as all subsets!
 
